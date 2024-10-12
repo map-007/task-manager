21aa384055a69ac247ae4c54ccf764be787312db
map007 <min.aungpaing007@gmail.com>
2024-10-12 23:10:06 +0630

    change: source code

--- backend/validations/category.js
+++
@@ -1,6 +0,0 @@
-const { check } = require("express-validator");
-
-//validate email name and password
-exports.categoryCreateValidator = [
-  check("name").not().isEmpty().withMessage("Category name is required!"),
-];
